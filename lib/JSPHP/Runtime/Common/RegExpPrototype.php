7312e3ee5c14f9cd32f8bf74ca1965e2e708aac0
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2012-12-16 18:52:20 +0100

    Typo

--- lib/JSPHP/Runtime/Common/RegExpPrototype.php
+++ lib/JSPHP/Runtime/Common/RegExpPrototype.php
@@ -4,8 +4,8 @@ require_once 'JSPHP/Runtime/Object.php';
 class JSPHP_Runtime_Common_RegExpPrototype {
     static function construct($regexp, $pattern, $flags = null) {
         $regexp['source'] = $pattern;
-        $regexp['global'] = strpos($flags, 'g') !== false);
-        $regexp['ignoreCase'] = strpos($flags, 'i') !== false);
-        $regexp['multiline'] = strpos($flags, 'm') !== false);
+        $regexp['global'] = strpos($flags, 'g') !== false;
+        $regexp['ignoreCase'] = strpos($flags, 'i') !== false;
+        $regexp['multiline'] = strpos($flags, 'm') !== false;
     }
 }
